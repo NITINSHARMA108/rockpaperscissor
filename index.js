5aeac5e2cbf9f67a3fb2f21c1675c14f1bc9b59c
nitin sharma <nitin.sharma@jalantechnologies.com>
2021-10-07 09:38:02 +0530

    final

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let resultcount=0;
+
+
+
 function computerPlay(){
     let array=['rock','paper','scissor'];
 
     return array[Math.floor(Math.random()*3)];
 }
 function result(userSelect,computerSelect){
+    
     if(userSelect=='rock')
     {
         if(computerSelect==userSelect)
@@ -52,19 +56,52 @@ function result(userSelect,computerSelect){
         }
     }
 }
+
+
 //starting the game for the user;
 function startGame(){
+   
+   
+        
+            for(var i=0;i<5;i++)
+            {
+                //setTimeout(function(){
+                    let uS=window.prompt('make your choice');
+            
+                    let userSelection=uS.toLowerCase();
+                    
+                    if(userSelection!=='rock')
+                    {
+                        if(userSelection!=='paper')
+                        {
+                            if(userSelection!=='scissor')
+                            {
+                                window.alert('incorrect value');
+                                i--;
+                                continue;
 
-    for(var i=0;i<5;i++)
-    {
-    let uS=window.prompt('make your choice');
-    let userSelection=uS.toLowerCase();
-    let computerSelection=computerPlay();
+                            }
+                        }
+                    
+                    }
+                    console.log('your selection '+userSelection);
+                    let computerSelection=computerPlay();
+                    console.log('computer selection '+computerSelection);
+                    
+                    let res=result(userSelection,computerSelection);
+                    console.log(res);
+                    setTimeout(10000);
+
+               // },1000);
+            
+            
+           
+        }
+            
+
+     
     
-    
-    console.log(result(userSelection,computerSelection));
-    }
-    console.log('Total wins '+resultcount+ ' out of 5');
+    console.log(`Total wins ${resultcount} out of 5`);
 }
 
 startGame();
